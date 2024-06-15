c59bb62662a6bdd0fdd53d34185efad3bbd30df7
SergeySadykovN <snsadykov@ya.ru>
2024-06-15 13:31:46 +0300

    add 2 posts and 1 user

--- board/admin.py
+++ board/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-from .models import User, Post, Comment, Category
+from .models import (User,
+                     Post,
+                     Comment,
+                     Category)
+
 
 admin.site.register(User)
+
+# @admin.register(Post)
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'user', 'created', 'content')
+#     list_filter = ('created', 'user')
+#     raw_id_fields = ('user',)
+#     date_hierarchy = 'created'
+#     ordering = ('created',)
+
+
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Category)
-
-
-# Register your models here.
--- board/models.py
+++ board/models.py
@@ -9,25 +9,29 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)  # title Post
     content = models.TextField()  # Content of post
     created = models.DateTimeField(auto_now_add=True)  # data and time created post
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # внешний ключ на пользлвателя
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # внешний ключ на пользлвателя
 
     def __str__(self):
         return self.title
 
 
 class Comment(models.Model):
-    content = models.TextField() # Контент коментария
-    created = models.DateTimeField(auto_now_add=True) # дата и время создания коментария
-    user = models.ForeignKey(User, on_delete=models.CASCADE) # внешний ключ на полььзвоателя
-    post = models.ForeignKey(Post, on_delete=models.CASCADE) # внешний ключ на пост
+    content = models.TextField()  # Контент коментария
+    created = models.DateTimeField(auto_now_add=True)  # дата и время создания коментария
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # внешний ключ на полььзвоателя
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)  # внешний ключ на пост
 
     def __str__(self):
-        return f'Comment by {self.user.username} on {self.post.title}'
+        return f'Comment by {self.author.username} on {self.post.title}'
 
 
 class Category(models.Model):
@@ -36,5 +40,3 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-
-
